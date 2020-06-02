8179ce3a5512e76c8865455231f878cfb20be0e3
Chen Luo <luochen1007@gmail.com>
2020-06-02 18:09:21 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Decrement from "./Decrement";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Decrement start={5} />
+        <Decrement start={10} />
+        <Decrement start={30} />
+      </>
+    );
+  }
+}
+
+export default App;
